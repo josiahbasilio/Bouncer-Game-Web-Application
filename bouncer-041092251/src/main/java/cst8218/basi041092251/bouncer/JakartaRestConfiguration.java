ad894679d3f688bc9e4e689699f9bb6f350f56c5
Matthew Christian <matthewchristian33@gmail.com>
2025-03-25 16:19:45 -0400

    Added database security annotations to bouncer app.

--- bouncer-041092251/src/main/java/cst8218/basi041092251/bouncer/JakartaRestConfiguration.java
+++ bouncer-041092251/src/main/java/cst8218/basi041092251/bouncer/JakartaRestConfiguration.java
@@ -4,10 +4,25 @@
  */
 package cst8218.basi041092251.bouncer;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+import jakarta.security.enterprise.authentication.mechanism.http.BasicAuthenticationMechanismDefinition;
+import jakarta.security.enterprise.identitystore.DatabaseIdentityStoreDefinition;
+import jakarta.security.enterprise.identitystore.Pbkdf2PasswordHash;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-// Initialize the RESTful API configuration
+// Initialize the RESTful API configuration.
+@BasicAuthenticationMechanismDefinition
+@DatabaseIdentityStoreDefinition(
+	dataSourceLookup = "${'java:app/MariaDB'}",
+	callerQuery = "#{'select password from appuser where userid = ?'}",
+	groupsQuery = "select groupname from appuser where userid = ?",
+	hashAlgorithm =  Pbkdf2PasswordHash.class,
+	priority = 10
+)
+@Named
+@ApplicationScoped
 @ApplicationPath("resources")
 public class JakartaRestConfiguration extends Application {
     
